6216033808161cfc28c22a9699e6623f516c1b7e
Hatry1337 <Hatrychannel@gmail.com>
2022-07-03 13:57:51 +0300

    sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './Header';
+import logo_glow from "./assets/logo_glow.png";
+import { Button, Container, Nav } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header>
+				<Nav.Link className="nav-texts" href="#home">Home</Nav.Link>
+				<Nav.Link className="nav-texts" href="#link">Link</Nav.Link>
+				<Nav.Link className="nav-texts" href="#home">Home</Nav.Link>
+				<Nav.Link className="nav-texts" href="#link">Link</Nav.Link>
+				<Nav.Link className="nav-texts" href="#home">Home</Nav.Link>
+				<Nav.Link className="nav-texts" href="#link">Link</Nav.Link>
+			</Header>
+			<section style={{ backgroundColor: "#623FFF" }}>
+				<img src={logo_glow} className="big-logo" alt="logo" draggable="false"/>
+				<h1 className="big-text">Imagine a BOT...</h1>
+			</section>
+			
+			
+			<section style={{ height: "160rem" }}>
+				<Container className='pt-2 pb-2 pl-2 pe-2'>
+					<Button className='me-2' variant='primary'>Suck Dick</Button>
+					<Button className='me-2' variant='secondary'>Lick Balls</Button>
+					<Button className='me-2' variant='success'>Lick Balls</Button>
+					<Button className='me-2' variant='danger'>Lick Balls</Button>
+					<Button className='me-2' variant='warning'>Lick Balls</Button>
+					<Button className='me-2' variant='info'>Lick Balls</Button>
+					<Button className='me-2' variant='light'>Lick Balls</Button>
+					<Button className='me-2' variant='dark'>Lick Balls</Button>
+
+					<br></br>
+					<br></br>
+
+					<Button className='me-2' variant='outline-primary'>Suck Dick</Button>
+					<Button className='me-2' variant='outline-secondary'>Lick Balls</Button>
+					<Button className='me-2' variant='outline-success'>Lick Balls</Button>
+					<Button className='me-2' variant='outline-danger'>Lick Balls</Button>
+					<Button className='me-2' variant='outline-warning'>Lick Balls</Button>
+					<Button className='me-2' variant='outline-info'>Lick Balls</Button>
+					<Button className='me-2' variant='outline-light'>Lick Balls</Button>
+					<Button className='me-2' variant='outline-dark'>Lick Balls</Button>
+				</Container>
+			</section>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,30 @@
+import React, { ReactElement } from 'react';
+import './Header.css';
+import './App.css';
+import logo from './assets/logo512.png'
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+interface HeaderProps{
+    children?: ReactElement | ReactElement[];
+}
+
+function Header(props: HeaderProps) {
+	return (
+        <Navbar className="navbar-bg-color pt-0 pb-0" expand="lg" sticky="top">
+            <Container>
+                <Navbar.Brand href="/" className="nav-texts pe-2">
+                    <img src={logo} className="d-inline-block align-middle" alt="logo" height={50} draggable="false"/>
+                    <span className="align-middle h3 text-white">RainbowBOT</span>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+                <Nav className="me-0">
+                    {props.children}
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+	);
+}
+
+export default Header;
